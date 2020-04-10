b4c1d19c0c5ac48a01255a600f1860a4b1ab44a9
Huseyn <your>
2020-04-10 12:24:53 +0300

    Built a class DocumentWorker

---
+++ Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+namespace HM_09_04_2020_SUM_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+        }
+         class DocumentWorker
+    {
+       public void OpenDocument()
+        {
+            Console.WriteLine("Document open");
+        }
+       public virtual void EditDocument()
+        {
+            Console.WriteLine("Document editing is available in version PRO");//Редактирование документа доступно в версии Пhj
+
+        }
+        public virtual void SaveDocument()
+        {
+            Console.WriteLine("Saving a document is available in version PRO");//Сохранение документа доступно в версии Про
+
+        }
+    }
+    }
+}
