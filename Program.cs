a256e8b2c49b4e1a25697ae013e205c4f382e9db
Huseyn <your>
2020-04-10 13:31:13 +0300

    Connect all class and inheritance to the Main method

--- Program.cs
+++ Program.cs
@@ -5,44 +5,36 @@ namespace HM_09_04_2020_SUM_1
     {
         static void Main(string[] args)
         {
-            
+            DocumentWorker dw;
+            Console.Write("Enter the key:");
+            string key =Console.ReadLine();
+            if (key=="pro")
+                dw = new ProDocumentWorker();
+            else if (key=="exp")
+                dw = new ExpertDocumentWorker();
+            else
+                dw = new DocumentWorker();
+            dw.OpenDocument();
+            dw.SaveDocument();
+            dw.EditDocument();
+            Console.ReadKey();
         }
     class DocumentWorker
     {
-       public void OpenDocument()
-        {
-            Console.WriteLine("Document open");
-        }
-       public virtual void EditDocument()
-        {
-            Console.WriteLine("Document editing is available in version PRO");//Редактирование документа доступно в версии Пhj
-
-        }
-        public virtual void SaveDocument()
-        {
-            Console.WriteLine("Saving a document is available in version PRO");//Сохранение документа доступно в версии Про
-
-        }
+        public void OpenDocument() => Console.WriteLine("Document open");
+        public virtual void EditDocument() => Console.WriteLine("Document editing is available in version PRO");
+        public virtual void SaveDocument() => Console.WriteLine("Saving a document is available in version PRO");
+       
     }
     class ProDocumentWorker: DocumentWorker
     {
-        public override void EditDocument()
-        {
-
-            Console.WriteLine("Document was edit!");
-
-        }
-        public override void SaveDocument()
-        {
-            Console.WriteLine("The document is saved in the old format, saving in other formats is available in the Expert version");
-        }
+        public override void EditDocument() => Console.WriteLine("Document was edit!");
+       
+        public override void SaveDocument() => Console.WriteLine("The document is saved in the old format, saving in other formats is available in the Expert version");
     }
     class ExpertDocumentWorker:ProDocumentWorker
     {
-        public override void SaveDocument()
-        {
-            Console.WriteLine("The document is saved in a new format");//Документ сохранен в новом формате 
-        }
+        public override void SaveDocument() => Console.WriteLine("The document is saved in a new format");
     }
     }
 }
